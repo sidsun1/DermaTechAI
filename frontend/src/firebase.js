1133f84a33345d4f33b4117e7bc29ed12a957c8b
Siddharth <111776145+sidsun1@users.noreply.github.com>
2025-01-25 10:49:39 -0800

    Delete frontend directory

--- frontend/src/firebase.js
+++
@@ -1,8 +0,0 @@
-import firebaseConfig from "./api-key.js"
-import firebase from "firebase/app";
-
-firebase.initializeApp(firebaseConfig)
-
-export const auth = firebase.auth();
-export const googleProvider = new firebase.auth.GoogleAuthProvider();
-export default firebase
